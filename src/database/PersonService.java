26b9d542eec5243fb8e9be13cd0f98f148ca96f8
Mahesh Sai <maheshsai252@gmail.com>
2022-12-10 07:43:50 -0500

    Added person service

---
+++ src/database/PersonService.java
@@ -0,0 +1,201 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import model.Person;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author maheshsai
+ */
+public class PersonService {
+     Connection conn;
+
+    public PersonService() {
+        fetch();
+    }
+    
+    private void fetch() {
+      
+        String connectionURL = "jdbc:derby://localhost:1527/sample"; //ConnectionURL, username and password should be specified in getConnection() 
+        try {
+            DriverManager.registerDriver(new org.apache.derby.jdbc.ClientDriver());
+
+            conn = DriverManager.getConnection(connectionURL, "app", "app"); 
+            System.out.println("Connect successfully ! ");
+            
+        } catch (SQLException ex) { 
+            System.out.println("Connect failed ! ");
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public ArrayList<Person> getAllPersons() {
+        ArrayList<Person> al = new ArrayList<>();
+        try {
+             String sql = "SELECT * FROM Person";
+             if(conn==null || conn.isClosed()) {
+                 fetch();
+             }
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            int count = 0;
+ 
+            while (result.next()){
+                String name = result.getString("name");
+                String id = result.getString("id");
+                String email = result.getString("email");
+                String password = result.getString("password");
+                String role = result.getString("role");
+                Person h = new Person(id,name,email,password,role);
+                al.add(h);
+            }
+        } catch (SQLException ex) { 
+            System.out.println("Connect failed ! ");
+            System.out.println(ex.getMessage());
+        }
+       
+        return al;
+
+    }
+    public String create(Person p) {
+        return this.insertPerson(p.getName(), p.getId(), p.getEmail(), p.getPassword(), p.getRole());
+    }
+    public String insertPerson(String name,String id, String email,String password,String role) {
+        try {
+                String sql = "INSERT INTO Person (name, id, email,password,role) VALUES (?, ?, ?,?,?)";
+                if(conn==null || conn.isClosed()) {
+                    fetch();
+                }
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setString(1, name);
+                statement.setString(2, id);
+                statement.setString(3, email);
+                statement.setString(4, password);
+                statement.setString(5, role);
+
+                int rowsInserted = statement.executeUpdate();
+                if (rowsInserted > 0) {
+                   return "A new erson was inserted successfully!";
+                }  else {
+                    return "got error";
+                }
+        } catch (SQLException ex) { 
+            System.out.println("Connect failed ! ");
+            return ex.getMessage();
+        }
+       
+
+    }
+    public String update(Person p) {
+        return this.updatePerson(p.getName(), p.getId(), p.getPassword());
+    }
+    public String updatePerson(String name,String id,String password) {
+        try {
+                String sql = "Update Person set name = ?, password = ? where id = ?";
+                if(conn==null || conn.isClosed()) {
+                    fetch();
+                }
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setString(1, name);
+                statement.setString(3, id);
+                statement.setString(2, password);
+ 
+                int rowsInserted = statement.executeUpdate();
+                if (rowsInserted > 0) {
+                   return "person was updated successfully!";
+                }  else {
+                    return "got error";
+                }
+        } catch (SQLException ex) { 
+            System.out.println("Connect failed ! ");
+            return ex.getMessage();
+        }
+       
+
+    }
+    public String deletePerson(String id) {
+        try {
+                String sql = "Delete from person where id = ?";
+                if(conn==null || conn.isClosed()) {
+                    fetch();
+                }
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setString(1, id);
+ 
+                int rowsInserted = statement.executeUpdate();
+                if (rowsInserted > 0) {
+                   return "person was deleted successfully!";
+                }  else {
+                    return "got error";
+                }
+        } catch (SQLException ex) { 
+            System.out.println("Connect failed ! ");
+            return ex.getMessage();
+        }
+
+    }
+    public Person validatePerson(String email, String Password) {
+        try {
+                String sql = "Select * from person where email = ?";
+                if(conn==null || conn.isClosed()) {
+                    fetch();
+                }
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setString(1, email);
+ 
+                ResultSet result = statement.executeQuery();
+                while(result.next()) {
+                    String name = result.getString("name");
+                    String emaile = result.getString("email");
+                    String password = result.getString("password");
+                    String role = result.getString("role");
+                    String id = result.getString("id");
+
+                    Person p = new Person(id,name,emaile,password,role);
+                    return p;
+                }
+                
+        } catch (SQLException ex) { 
+            System.out.println("Connect failed ! "+ ex.getMessage());
+            return null;
+        }
+        return null;
+    }
+    public Person getPerson(String id) {
+        try {
+                String sql = "Select * from person where id = ?";
+                if(conn==null || conn.isClosed()) {
+                    fetch();
+                }
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setString(1, id);
+ 
+                ResultSet result = statement.executeQuery();
+                while(result.next()) {
+                    String name = result.getString("name");
+                    String emaile = result.getString("email");
+                    String password = result.getString("password");
+                    String role = result.getString("role");
+
+                    Person p = new Person(id,name,emaile,password,role);
+                    return p;
+                }
+                
+        } catch (SQLException ex) { 
+            System.out.println("Connect failed ! "+ ex.getMessage());
+            return null;
+        }
+        return null;
+    }
+}
