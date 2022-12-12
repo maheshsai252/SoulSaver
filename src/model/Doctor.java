fcbcaef4d8d7d00562cbdb5e984dc7600d45c2d1
Somesh1412 <ramisetty.s@northeastern.edu>
2022-12-11 21:16:55 -0500

    Fixed doctor bugs

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -4,71 +4,119 @@
  */
 package model;
 
+import database.HospitalDBService;
+import java.util.ArrayList;
+import java.util.UUID;
+
 /**
  *
  * @author somes
  */
-public class Doctor {
-     private String docID;
-    private String docName;
-    private String hospID;
-    private String docUsername;
-    private String docPass;
+public class Doctor extends Person {
+    private String hospitalId;
+   private String doctororg_id;
 
-    public Doctor(String docID, String docName, String hospID) {
-        this.docID = docID;
-        this.docName = docName;
-        this.hospID = hospID;
+    public String getDoctororg_id() {
+        return doctororg_id;
     }
 
-    public String getDocUsername() {
-        return docUsername;
+    public void setDoctororg_id(String doctororg_id) {
+        this.doctororg_id = doctororg_id;
     }
 
-    public void setDocUsername(String docUsername) {
-        this.docUsername = docUsername;
+    private Hospital hospital;
+    private EncounterHistory encounterHistory; 
+    private double consultation_charges;
+    private double surgery_charges;
+
+    public double getConsultation_charges() {
+        return consultation_charges;
     }
 
-    public String getDocPass() {
-        return docPass;
+    public void setConsultation_charges(double consultation_charges) {
+        this.consultation_charges = consultation_charges;
     }
 
-    public void setDocPass(String docPass) {
-        this.docPass = docPass;
+    public double getSurgery_charges() {
+        return surgery_charges;
     }
-    
-    public Doctor() {
-        this.docID = "123";
-        this.docName = "Rahul";
-        this.hospID = "3456";
+
+    public void setSurgery_charges(double surgery_charges) {
+        this.surgery_charges = surgery_charges;
     }
     
+    public Doctor(String name, String password, String userName) {
+       super(name,password,userName,"Doctor");
+       this.id = UUID.randomUUID().toString();
 
-    public String getDocID() {
-        return docID;
     }
-
-    public void setDocID(String docID) {
-        this.docID = docID;
+    public Doctor(String id, String name, String email, String password) {
+       super(id,name,email,password,"Doctor");
+       
     }
-
-    public String getDocName() {
-        return docName;
-    }
-
-    public void setDocName(String docName) {
-        this.docName = docName;
-    }
-
-    public String getHospID() {
-        return hospID;
-    }
-
-    public void setHospID(String hospID) {
-        this.hospID = hospID;
+    public Doctor(ArrayList<String> userarray) {
+        
+        super(userarray.get(1),userarray.get(3),userarray.get(2),"Doctor");
+        System.out.println(userarray);
+        this.setId(userarray.get(0));
+        this.setHospitalId(userarray.get(4));
     }
     @Override
-    public String toString(){
-        return docName;
+    public String toString() {
+        if(this.name.equals(null)) {
+            return "NA";
+        }
+        return this.getName();
     }
+     public EncounterHistory getEncounterHistory() {
+        if(this.encounterHistory == null) {
+            this.encounterHistory = new EncounterHistory(id, false);
+        }
+        return encounterHistory;
+    }
+    public void setEncounterHistory(EncounterHistory encounterHistory) {
+        this.encounterHistory = encounterHistory;
+    }
+    public String getHospitalId() {
+        return hospitalId;
+    }
+
+    public void setHospitalId(String hospitalId) {
+        System.out.println(hospitalId+"Setting");
+        this.hospitalId = hospitalId;
+    }
+
+    public Hospital getHospital() {
+        if(this.hospital == null ) {
+            System.out.print(this.hospitalId);
+            HospitalDBService hs = new HospitalDBService();
+            return hs.getHospital(this.hospitalId);
+        }
+        return hospital;
+    }
+//    public Community getCommunity() {
+//        if(this.hospitalId != null ) {
+//            HospitalService hs = new HospitalService();
+//            Hospital h = hs.getHospital(this.hospitalId);
+//            if(h!=null) {
+//                return h.getCommunity();
+//            }
+//        }
+//        return null;
+//    }
+
+    public void setHospital(Hospital hospital) {
+        this.hospital = hospital;
+        this.hospitalId = hospital.getId();
+    }
+    
+    public boolean verify(String password){
+        if(password.equals(getPassword())) {
+            return true;
+        }
+        return false;
+    }
+
+    
+
 }
